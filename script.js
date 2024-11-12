cd19313853eb44358cdd2c40e4c91b9136d81542
alejandro lopez lugris <you@example.com>
2024-11-12 12:40:52 +0100

    cambios de diseño y links

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const nav = document.querySelector(".links");
 const github_url = "https://github.com/alexlu21"
 const linkedin_url = "https://www.linkedin.com/in/alex-lugris-572a5b317/"
 const ubi_url = "https://www.google.com/maps/place/A+Coru%C3%B1a,+La+Coru%C3%B1a/@43.3618723,-8.4332832,6425m/data=!3m2!1e3!4b1!4m6!3m5!1s0xd2e7cfcf174574d:0x6a47350d095cdfee!8m2!3d43.3637626!4d-8.4103923!16zL20vMDJodmp5?entry=ttu&g_ep=EgoyMDI0MDkwNC4wIKXMDSoASAFQAw%3D%3D"
-const flags_game_url = "https://github.com/alexlu21/Flags-game"
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x')
@@ -17,9 +16,6 @@ function linkedin(){
 }
 function ubi(){
     window.open(ubi_url,'_blank')
-}
-function flags_game() {
-    window.open(flags_game_url,'_blank')
 }
 function redi(url){
     window.open(url,'_blank')
